4fcfc5ce26dc8ec36e60d77645bf332753467095
VitaliyBondarenko1982 <vitaliybondarenko@ukr.net>
2018-04-23 21:03:35 +0300

    breaks the code into modules

---
+++ js/utils.js
@@ -0,0 +1,40 @@
+'use strict';
+
+
+(function () {
+  window.utils = {
+    // Функция,возвращающая случайное значение из заданого интервала
+    getRandomNumber: function (min, max) {
+      return Math.round(Math.random() * (max - min + 1) + min - 0.5);
+    },
+
+    // Функция, возвращающая случайный елемент из массива
+    getRandomElement: function (array) {
+      var randomIndex = window.utils.getRandomNumber(0, array.length - 1);
+      var randomElement = array[randomIndex];
+      return randomElement;
+    },
+
+    getArrayFromArray: function (array, length) {
+      length = length ? length : window.utils.getRandomNumber(1, array.length);
+      var newArray = [];
+
+      while (newArray.length < length) {
+        pushArray(array, newArray);
+      }
+
+      return newArray;
+    }
+  };
+
+  var pushArray = function (array, newArray) {
+    var secondComments = window.utils.getRandomElement(array);
+
+    if (newArray.indexOf(secondComments) !== -1) {
+      pushArray(array, newArray);
+    } else {
+      newArray.push(secondComments);
+    }
+  };
+
+})();
